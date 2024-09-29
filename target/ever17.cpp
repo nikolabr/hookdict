d21c0a18576cf4d6ade3df7fecfc4ba64f7034c4
nikolabr <nikolabrk@protonmail.com>
2024-09-29 02:31:05 +0200

    BitBlt hook works

--- target/ever17.cpp
+++ target/ever17.cpp
@@ -9,173 +9,165 @@
 #include <string>
 
 #include <boost/interprocess/mapped_region.hpp>
-#include <wingdi.h>
 #include <gdiplus.h>
+#include <wingdi.h>
 
 using namespace targets::kid;
 using namespace Gdiplus;
 
 static std::shared_ptr<ever17> g_ever17 = nullptr;
 static ULONG_PTR gdiplusToken;
+static std::atomic<unsigned int> counter = 0;
 
-HRESULT GetEncoderClsid(const std::wstring& format, GUID* pGuid)
-{
-	HRESULT hr = S_OK;
-	UINT  nEncoders = 0;          // number of image encoders
-	UINT  nSize = 0;              // size of the image encoder array in bytes
-	std::vector<BYTE> spData;
-	Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
-	Gdiplus::Status status;
-	bool found = false;
+HRESULT GetEncoderClsid(const std::wstring &format, GUID *pGuid) {
+  HRESULT hr = S_OK;
+  UINT nEncoders = 0; // number of image encoders
+  UINT nSize = 0;     // size of the image encoder array in bytes
+  std::vector<BYTE> spData;
+  Gdiplus::ImageCodecInfo *pImageCodecInfo = NULL;
+  Gdiplus::Status status;
+  bool found = false;
 
-	if (format.empty() || !pGuid)
-	{
-		hr = E_INVALIDARG;
-	}
+  if (format.empty() || !pGuid) {
+    hr = E_INVALIDARG;
+  }
 
-	if (SUCCEEDED(hr))
-	{
-		*pGuid = GUID_NULL;
-		status = Gdiplus::GetImageEncodersSize(&nEncoders, &nSize);
+  if (SUCCEEDED(hr)) {
+    *pGuid = GUID_NULL;
+    status = Gdiplus::GetImageEncodersSize(&nEncoders, &nSize);
 
-		if ((status != Gdiplus::Ok) || (nSize == 0))
-		{
-			hr = E_FAIL;
-		}
-	}
+    if ((status != Gdiplus::Ok) || (nSize == 0)) {
+      hr = E_FAIL;
+    }
+  }
 
-	if (SUCCEEDED(hr))
-	{
+  if (SUCCEEDED(hr)) {
 
-		spData.resize(nSize);
-		pImageCodecInfo = (Gdiplus::ImageCodecInfo*)&spData.front();
-		status = Gdiplus::GetImageEncoders(nEncoders, nSize, pImageCodecInfo);
+    spData.resize(nSize);
+    pImageCodecInfo = (Gdiplus::ImageCodecInfo *)&spData.front();
+    status = Gdiplus::GetImageEncoders(nEncoders, nSize, pImageCodecInfo);
 
-		if (status != Gdiplus::Ok)
-		{
-			hr = E_FAIL;
-		}
-	}
+    if (status != Gdiplus::Ok) {
+      hr = E_FAIL;
+    }
+  }
 
-	if (SUCCEEDED(hr))
-	{
-		for (UINT j = 0; j < nEncoders && !found; j++)
-		{
-			if (pImageCodecInfo[j].MimeType == format)
-			{
-				*pGuid = pImageCodecInfo[j].Clsid;
-				found = true;
-			}
-		}
+  if (SUCCEEDED(hr)) {
+    for (UINT j = 0; j < nEncoders && !found; j++) {
+      if (pImageCodecInfo[j].MimeType == format) {
+        *pGuid = pImageCodecInfo[j].Clsid;
+        found = true;
+      }
+    }
 
-		hr = found ? S_OK : E_FAIL;
-	}
+    hr = found ? S_OK : E_FAIL;
+  }
 
-	return hr;
+  return hr;
 }
 
-common::shared_memory* targets::kid::ever17::get_shm_ptr() {
-	void* p = m_region.get_address();
-	return static_cast<common::shared_memory*>(p);
+common::shared_memory *targets::kid::ever17::get_shm_ptr() {
+  void *p = m_region.get_address();
+  return static_cast<common::shared_memory *>(p);
 }
 
 std::shared_ptr<ever17>
-targets::kid::ever17::try_create(hook_manager& hm,
-	boost::interprocess::mapped_region&& region) {
-	std::filesystem::path module_path = common::get_module_file_name_w();
+targets::kid::ever17::try_create(hook_manager &hm,
+                                 boost::interprocess::mapped_region &&region) {
+  std::filesystem::path module_path = common::get_module_file_name_w();
 
-	auto filename = std::move(module_path).filename().wstring();
-	std::transform(filename.begin(), filename.end(), filename.begin(), towupper);
+  auto filename = std::move(module_path).filename().wstring();
+  std::transform(filename.begin(), filename.end(), filename.begin(), towupper);
 
-	if (filename != L"EVER17PC.EXE") {
-		return nullptr;
-	}
+  if (filename != L"EVER17PC.EXE") {
+    return nullptr;
+  }
 
-	auto ptr = std::make_shared<ever17>();
-	g_ever17 = ptr;
+  auto ptr = std::make_shared<ever17>();
+  g_ever17 = ptr;
 
-	ptr->m_region = std::move(region);
-	ptr->m_textoutw_hook.enable(hm, textoutw_hook::fake_call);
+  ptr->m_region = std::move(region);
+  ptr->m_textoutw_hook.enable(hm, textoutw_hook::fake_call);
+  ptr->m_bitblt_hook.enable(hm, bitblt_hook::fake_call);
 
-	auto* shm_ptr = ptr->get_shm_ptr();
-	send_message(shm_ptr,
-		common::target_connected_message_t{ .m_window_name{
-			"Ever17 - the out of infinity -  Premium Edition DVD"} });
+  auto *shm_ptr = ptr->get_shm_ptr();
+  send_message(shm_ptr,
+               common::target_connected_message_t{.m_window_name{
+                   "Ever17 - the out of infinity -  Premium Edition DVD"}});
 
-	ptr->m_main_wnd = ::FindWindow(
-		nullptr, "Ever17 - the out of infinity -  Premium Edition DVD");
-	if (ptr->m_main_wnd == NULL) {
-		throw std::runtime_error("Could not find main window!");
-	}
+  ptr->m_main_wnd = ::FindWindow(
+      nullptr, "Ever17 - the out of infinity -  Premium Edition DVD");
+  if (ptr->m_main_wnd == NULL) {
+    throw std::runtime_error("Could not find main window!");
+  }
 
-	GdiplusStartupInput gdiplusStartupInput;
-	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+  GdiplusStartupInput gdiplusStartupInput;
+  GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
-	return ptr;
+  return ptr;
 }
 
 ever17::textoutw_hook::return_t WINAPI ever17::textoutw_hook::fake_call(
-	HDC hdc, int x, int y, LPCSTR lpString, int c) {
-	std::string out(lpString);
-	uint32_t codepage = GetACP();
+    HDC hdc, int x, int y, LPCSTR lpString, int c) {
+  std::string out(lpString);
+  uint32_t codepage = GetACP();
 
-	auto* shm_ptr = g_ever17->get_shm_ptr();
-	send_message(shm_ptr, common::target_generic_message_t{ .m_text{lpString},
-														   .m_cp{codepage} });
+  auto *shm_ptr = g_ever17->get_shm_ptr();
+  send_message(shm_ptr, common::target_generic_message_t{.m_text{lpString},
+                                                         .m_cp{codepage}});
 
-	int res = g_ever17->m_textoutw_hook.m_old_ptr(hdc, x, y, lpString, c);
+  int res = g_ever17->m_textoutw_hook.m_old_ptr(hdc, x, y, lpString, c);
 
-	if (g_ever17->m_main_wnd != NULL) {
-		/*
-		RECT rect;
-		::GetClientRect(g_ever17->m_main_wnd, &rect);
+  if (g_ever17->m_main_wnd != NULL) {
 
-		HDC hdc = ::GetDC(g_ever17->m_main_wnd);
+  }
 
-		DWORD width = rect.right - rect.left;
-		DWORD height = rect.bottom - rect.top;
-
-		Bitmap* bmp = new Bitmap(::CreateCompatibleBitmap(hdc, width, height), 0);
-		Graphics bmpGraphics(bmp);
-
-		HDC hBmpDC = bmpGraphics.GetHDC();
-		DWORD lines = ::BitBlt(hBmpDC, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
-
-		CLSID clsid;
-		GetEncoderClsid(L"image/bmp", &clsid);
-		bmp->Save(L"image.bmp", &clsid, nullptr);
-
-		send_message(shm_ptr, common::bitmap_update_message_t{
-			.m_width{(long)width},
-			.m_height{(long)height},
-			.m_lines{lines}
-			});
-
-		::ReleaseDC(NULL, hdc);
-		*/
-	}
-
-	return res;
+  return res;
 }
 
-ever17::bitblt_hook::return_t ever17::bitblt_hook::fake_call(HDC   hdc,
-	int   x,
-	int   y,
-	int   cx,
-	int   cy,
-	HDC   hdcSrc,
-	int   x1,
-	int   y1,
-	DWORD rop) {
+ever17::bitblt_hook::return_t
+ever17::bitblt_hook::fake_call(HDC hdc, int x, int y, int cx, int cy,
+                               HDC hdcSrc, int x1, int y1, DWORD rop) {
+  
+  auto res = g_ever17->m_bitblt_hook.m_old_ptr(hdc, x, y, cx, cy, hdcSrc, x1, y1, rop);
+  auto *shm_ptr = g_ever17->get_shm_ptr();
+    
+  RECT rect;
+  ::GetClientRect(g_ever17->m_main_wnd, &rect);
 
+  DWORD width = rect.right - rect.left;
+  DWORD height = rect.bottom - rect.top;
 
+  Bitmap* bmp = new Bitmap(::CreateCompatibleBitmap(hdc, width, height), 0);
+  Graphics bmpGraphics(bmp);
+
+  HDC hBmpDC = bmpGraphics.GetHDC();
+  DWORD lines = g_ever17->m_bitblt_hook.m_old_ptr(hBmpDC, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
+
+  CLSID clsid;
+  GetEncoderClsid(L"image/bmp", &clsid);
+  
+  std::wostringstream woss;
+  woss << "image" << counter << ".bmp";
+  std::wstring ws = woss.str();
+  
+  bmp->Save(ws.c_str(), &clsid, nullptr);
+  counter++;
+
+  send_message(shm_ptr, common::bitmap_update_message_t{
+      .m_width{(long)width},
+      .m_height{(long)height},
+      .m_lines{lines}
+    });
+  
+  return res;
 }
 
 ever17::~ever17() {
-	auto* shm_ptr = get_shm_ptr();
-	send_message(shm_ptr, common::target_close_message_t{});
+  auto *shm_ptr = get_shm_ptr();
+  send_message(shm_ptr, common::target_close_message_t{});
 
-	GdiplusShutdown(gdiplusToken);
+  GdiplusShutdown(gdiplusToken);
 
-	g_ever17 = nullptr;
+  g_ever17 = nullptr;
 }
--- target/ever17.h
+++ target/ever17.h
@@ -8,47 +8,44 @@
 #include <type_traits>
 
 namespace targets {
-	namespace kid {
-		class ever17
-		{
-			std::string m_prev;
+namespace kid {
+class ever17 {
+  std::string m_prev;
 
-			boost::interprocess::mapped_region m_region;
-		public:
-			struct textoutw_hook : hooks::hook_base<decltype(&TextOutA), HDC, int, int, LPCSTR, int> {
-				using hook_base::hook_base;
-				using return_t = hook_base::return_t;
+  boost::interprocess::mapped_region m_region;
 
-				static return_t WINAPI fake_call(HDC hdc, int x, int y, const char* lpString, int c);
-			};
+public:
+  struct textoutw_hook
+      : hooks::hook_base<decltype(&TextOutA), HDC, int, int, LPCSTR, int> {
+    using hook_base::hook_base;
+    using return_t = hook_base::return_t;
 
-			struct bitblt_hook : hooks::hook_base<decltype(&BitBlt), HDC, int, int, int, int, HDC, int, int, DWORD> {
-				using hook_base::hook_base;
-				using return_t = hook_base::return_t;
+    static return_t WINAPI fake_call(HDC hdc, int x, int y,
+                                     const char *lpString, int c);
+  };
 
-				static return_t WINAPI fake_call(
-					HDC   hdc,
-					int   x,
-					int   y,
-					int   cx,
-					int   cy,
-					HDC   hdcSrc,
-					int   x1,
-					int   y1,
-					DWORD rop
-				);
-			};
+  struct bitblt_hook : hooks::hook_base<decltype(&BitBlt), HDC, int, int, int,
+                                        int, HDC, int, int, DWORD> {
+    using hook_base::hook_base;
+    using return_t = hook_base::return_t;
 
-			common::shared_memory* get_shm_ptr();
+    static return_t WINAPI fake_call(HDC hdc, int x, int y, int cx, int cy,
+                                     HDC hdcSrc, int x1, int y1, DWORD rop);
+  };
 
-			HBITMAP m_hbm = NULL;
-			std::atomic<HWND> m_main_wnd = NULL;
+  common::shared_memory *get_shm_ptr();
 
-			static std::shared_ptr<ever17> try_create(hook_manager& hm, boost::interprocess::mapped_region&& region);
+  HBITMAP m_hbm = NULL;
+  std::atomic<HWND> m_main_wnd = NULL;
 
-			~ever17();
-		private:
-			textoutw_hook m_textoutw_hook{ TextOutA };
-		};
-	}
-}
+  static std::shared_ptr<ever17>
+  try_create(hook_manager &hm, boost::interprocess::mapped_region &&region);
+
+  ~ever17();
+
+private:
+  textoutw_hook m_textoutw_hook{TextOutA};
+  bitblt_hook m_bitblt_hook{BitBlt};
+};
+} // namespace kid
+} // namespace targets
